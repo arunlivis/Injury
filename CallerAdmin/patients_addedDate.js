42d602bc5d6b8b98ecc9b828cf65492a7fd92201
arunlivis <arunliviss@deemsysinc.com>
2016-09-28 11:20:09 +0530

    Commit for in Patients_added date

---
+++ CallerAdmin/patients_addedDate.js
@@ -0,0 +1,62 @@
+var checkPatient=require('../Helpers/checkPatients');
+var patientsClick=require('../Helpers/clickPatients');
+var urlPage=require('../Helpers/urlPage');
+var login=require('../Helpers/toLoginPage');
+var countyName=require('../Helpers/getCounty');
+
+describe('Caller Admin', function() {
+    it('Search Crash Report with Crash Date', function () {
+        urlPage.urlPage();
+        login.loginPage('calleradmin', 'calleradmin');
+        patientsClick.clickPatients();
+
+        var dates=[];
+        var datesMonth=[];
+        var datesDate=[];
+        var oneDay = 24*60*60*1000;
+        var endDate;
+        browser.sleep(600);
+        browser.driver.executeScript('window.scrollTo(0,0);');
+        element(by.partialLinkText('Reset')).click();
+        browser.waitForAngular();
+        element(by.name('addedOnFromDate')).click();
+        element(by.xpath('html/body/div[4]/div/div[1]/table/thead/tr[1]/th[2]')).click();
+        element(by.xpath('html/body/div[4]/div/div[2]/table/tbody/tr/td/span[5]')).click();
+        element(by.xpath('html/body/div[4]/div/div[1]/table/tbody/tr[3]/td[7]')).click();
+        browser.sleep(600);
+        element(by.name('addedOnToDate')).click();
+        element(by.xpath('html/body/div[5]/div/div[1]/table/thead/tr[1]/th[2]')).click();
+        element(by.xpath('html/body/div[5]/div/div[2]/table/tbody/tr/td/span[5]')).click();
+        element(by.xpath('html/body/div[5]/div/div[1]/table/tbody/tr[6]/td[3]')).click();
+        element(by.linkText('Search')).click();
+        browser.waitForAngular();
+        element(by.name('addedOnToDate')).getAttribute('value').then(function(toDate) {
+            endDate = new Date(toDate);
+        });
+        element(by.name('addedOnFromDate')).getAttribute('value').then(function(fromDate){
+            var startDate=new Date(fromDate);
+            var diffDays = Math.round(Math.abs((startDate.getTime() - endDate.getTime()) / (oneDay)));
+            for(i=0;i<diffDays;i++){
+                var tomorrow = new Date(startDate);
+                tomorrow.setDate(startDate.getDate()+i);
+                dates.push(tomorrow.toLocaleDateString());
+                if(dates[i].substring(1,2)=='/'){
+                    datesMonth.push('0'+dates[i]);
+                }
+                else{
+                    datesMonth.push(dates[i]);
+                }
+            }
+            for(j=0;j<diffDays;j++){
+                if(datesMonth[j].substring(4,5)=='/'){
+                    datesDate.push(datesMonth[j]);
+                    datesDate[j]=datesDate[j].substring(0,3)+'0'+datesDate[j].substring(3,9);
+                }
+                else{
+                    datesDate.push(datesMonth[j]);
+                }
+            }
+            checkPatient.checkPatient('calleradmin', 3, datesDate);
+        });
+    },999999);
+});
